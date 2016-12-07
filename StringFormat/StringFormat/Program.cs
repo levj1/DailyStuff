cdeb0e5b183023f4d72855f244065165c594b35d
levj1 <levj1@yahoo.com>
2016-12-07 16:54:32 -0500

    add contact class and contact repository

---
+++ StringFormat/StringFormat/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        //T1000\SQLEXPRESS(T1000\JamesLeveille)
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=T1000\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("select * from contact", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString() + " - " + reader[1].ToString());
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
+
